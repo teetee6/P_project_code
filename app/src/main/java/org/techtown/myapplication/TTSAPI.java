b70056442183d0e1497a4d6e3f17da73990fb71f
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2019-12-08 20:41:27 +0900

    keys.xml
    keys.xml

--- app/src/main/java/org/techtown/myapplication/TTSAPI.java
+++ app/src/main/java/org/techtown/myapplication/TTSAPI.java
@@ -20,19 +20,17 @@ import java.net.URL;
 import java.net.URLEncoder;
 
 public class TTSAPI {
+    static Context ctx;
     public static void main(String[] args){
         try {
-            Log.d("tag","hey2");
+            String apiId = ctx.getResources().getString(R.string.apiId);
+            String apiKey = ctx.getResources().getString(R.string.apiKey);
+            Log.d("tag",apiId);
             URL url = new URL("https://api.maum.ai/tts/stream");
             String text = URLEncoder.encode(args[0],"UTF-8");
             HttpURLConnection myConnection = (HttpURLConnection) url.openConnection();
-            myConnection.setRequestMethod("POST");//set post
-            //myConnection.setRequestProperty("Content-Type","application/json");
-            /*myConnection.setRequestProperty("apiId", "gachon.pproject.6728d71a1dc3c");
-            myConnection.setRequestProperty("apiKey", "ed13742e94834e339b70dc8a29a2142f");
-            myConnection.setRequestProperty("text", text);
-            myConnection.setRequestProperty("voiceName", "baseline_kor");*/
-            String postParams = "apiId=gachon.pproject.6728d71a1dc3c&apiKey=ed13742e94834e339b70dc8a29a2142f&voiceName=baseline_kor&text="+text;
+            myConnection.setRequestMethod("POST");
+            String postParams = "apiId="+apiId+"&apiKey="+apiKey+"&voiceName=baseline_kor&text="+text;
             myConnection.setDoInput(true);
             myConnection.setDoOutput(true);//send post data to OutputStream
             DataOutputStream wr = new DataOutputStream(myConnection.getOutputStream());
@@ -44,24 +42,6 @@ public class TTSAPI {
             BufferedReader br;
             if(myConnection.getResponseCode() == 200){
                 //success
-                            /*
-                            InputStream responseBody = myConnection.getInputStream();
-                            InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
-                            JsonReader jsonReader = new JsonReader(responseBodyReader);
-                            jsonReader.beginObject();
-
-                            while(jsonReader.hasNext()){
-                                String key = jsonReader.nextName();
-                                if(key.equals("")){
-                                    String value = jsonReader.nextString();
-                                    //
-                                    break;
-                                }else{
-                                    jsonReader.skipValue();//skip values of other keys
-                                }
-                            }
-                            jsonReader.close();
-                            myConnection.disconnect();*/
 
                 InputStream is = myConnection.getInputStream();
                 int read =0;
@@ -107,7 +87,6 @@ public class TTSAPI {
             @Override
             public void run() {
                 //networking logic should be here
-
             }
         });
     }
